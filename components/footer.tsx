152409c767848706e3750ad1dfe719ec64805691
Dev-ayansharma <ayansharma132006@gmail.com>
2026-05-06 20:51:33 +0530

    added privacy page4

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 const footerLinks = {
   Resources: ["Documentation"],
   Company: ["Contact"],
-  Legal: ["Terms","privacy"],
+  Legal: ["Terms","Privacy"],
 }
 
 export function Footer() {
